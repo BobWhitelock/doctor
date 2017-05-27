e63b3014164551759395534edb86a58b9d2e8653
Bob Whitelock <bob.whitelock1@gmail.com>
2017-05-27 20:31:53 +0100

    Output docs via pager depending on length

--- src/doctor.py
+++ src/doctor.py
@@ -1,6 +1,7 @@
 
 from click import Choice
 import click
+import shutil
 
 import docs
 import doc_parser
@@ -15,4 +16,21 @@ def doctor(language, search_term):
     with docs_entry.path.open() as f:
         parsed_doc = doc_parser.parse(f, docs_entry)
 
-    click.echo(parsed_doc)
+    echo_maybe_via_pager(parsed_doc)
+
+
+def echo_maybe_via_pager(text):
+    """Output text via pager or without, depending on terminal size."""
+    fallback_terminal_size = (80, 20)
+    _columns, lines = shutil.get_terminal_size(fallback_terminal_size)
+
+    # Still want to output via pager if text is almost at terminal size, to
+    # account for lines for prompt and feels better.
+    buffer_lines = 3
+
+    text_lines = len(text.splitlines())
+
+    if lines - buffer_lines < text_lines:
+        click.echo_via_pager(text)
+    else:
+        click.echo(text)
